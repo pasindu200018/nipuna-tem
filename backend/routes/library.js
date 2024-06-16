69d77618d06e1d7c77e51fc34022a949e2d48071
pasindu200018 <167278835+pasindu200018@users.noreply.github.com>
2024-06-16 15:34:40 +0530

    up

--- backend/routes/library.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const libraryController = require("../controllers/library");
-const router = express.Router();
-
-router.get("/get", libraryController.getLibraries);
-router.post("/create", libraryController.createLibrary);
-
-module.exports = router;
